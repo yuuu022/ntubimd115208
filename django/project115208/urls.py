c97d6cfb5e8592fd9a1fd502f4bfa650aabea8d3
11436032 <11436032@ntub.edu.tw>
2026-05-29 01:50:15 +0800

    修改QA頭像

--- django/project115208/urls.py
+++ django/project115208/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('/logo/', document_root=settings.BASE_DIR.parent / 'logo')
